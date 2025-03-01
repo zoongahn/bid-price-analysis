8fb6e89ba8ff25befb8b944f54cb1dca93278504
JOONGHYUN AHN <anjungh0103@gmail.com>
2025-03-01 16:00:07 +0000

    completed show api info

--- backend/db_config/production.py
+++ backend/db_config/production.py
@@ -4,14 +4,14 @@ import os
 
 def init_mongodb():
     """ 운영 환경에서 직접 MongoDB에 접속 """
-    DB_HOST = os.getenv("DB_HOST", "your-server-ip")
-    DB_PORT = int(os.getenv("DB_PORT", "27017"))
-    DB_USERNAME = quote_plus(os.getenv("DB_USERNAME", "prod_user"))
-    DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "prod_password"))
-    DB_NAME = os.getenv("DB_NAME", "gfcon")
+    DB_HOST = os.getenv("MONGODB_HOST")
+    DB_PORT = os.getenv("MONGODB_PORT")
+    DB_USERNAME = quote_plus(os.getenv("MONGODB_USER"))
+    DB_PASSWORD = quote_plus(os.getenv("MONGODB_PASSWORD"))
+    DB_AUTH_SOURCE = os.getenv("MONGODB_AUTH_SOURCE")
 
-    client = MongoClient(f"mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}")
+    client = MongoClient(f"mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/?authSource={DB_AUTH_SOURCE}")
 
-    db = client.get_database(DB_NAME)
+    db = client.get_database("gfcon")
 
     return db
