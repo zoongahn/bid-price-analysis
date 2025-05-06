30be6f4f8555b17114d18bedc3cb3202b1403c94
JOONGHYUN AHN <anjungh0103@gmail.com>
2025-05-06 23:54:52 +0900

    tmp commit

--- api-fetcher/scripts/run_all_years.sh
+++ api-fetcher/scripts/run_all_years.sh
@@ -2,9 +2,5 @@
 
 for year in {2010..2024}
 do
-<<<<<<< HEAD
-  screen -dmS "year_$year" bash -c "cd /data/dev/bid-price-analysis/api-fetcher && source ./.venv/bin/activate && python src/main.py --year $year; exec bash"
-=======
-  screen -dmS "year_$year" bash -c "python3 collect_bid.py --service 낙찰정보서비스 --oper 10 --year $year; exec bash"
->>>>>>> 42cae2062f14d779b33158f41e5652c75257dcc1
+  screen -dmS "year_$year" bash -c "cd /data/dev/bid-price-analysis/api-fetcher && source ./.venv/bin/activate && python fetch-data/collect_bid.py --service 낙찰정보서비스 --oper 10 --year $year; exec bash"
 done
